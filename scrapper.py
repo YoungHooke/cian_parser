dbb96acba7d7d4360bf70bcc7c6e5cb8b432f725
Alexander <114604140+YoungHooke@users.noreply.github.com>
2025-11-25 19:22:42 +0300

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -12,81 +12,91 @@ headers = {
         "Host" : "nn.cian.ru",
         "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0"
     }
-url = "https://nn.cian.ru/snyat-kvartiru/"
+
 
 #запрос на сайт для получения главной html-страницы
-def download_html_page(): 
-    responce = requests.Session().get(url, headers=headers)
-    cookies = responce.cookies.get_dict()
-    time.sleep(random.randint(1, 5))
+def main_page_download(counter, url, cookies):
     responce = requests.Session().get(url, headers=headers, cookies=cookies)
-
+    time.sleep(random.randint(2, 4))
     if responce.status_code == 200:
-        with open("cian_page.html", "w", encoding="utf8") as file:
-            file.write(responce.text)
-
+        return responce.text
     else:
         print(f"Error: responce status code : {responce.status_code}")
+        return None
 
-#скачиваем все страницы аренды квартир
-def page_parsing_download() :
-    with open ('cian_page.html', "r", encoding="utf-8") as file:
-        source = file.read()
+    
+#скачиваем все страницы аренды квартир и сразу обрабатываем
+def flat_pages__download(source, cookies):
     soap = BeautifulSoup(source, 'lxml')
     links = soap.find("div", class_="x31de4314--fb02f2--wrapper").find_all("a", class_="x31de4314--_416c6--media")
-    counter = 0
-    responce = requests.Session().get(url, headers=headers)
-    cookies = responce.cookies.get_dict()
-    time.sleep(random.randint(1, 5))
-
-    counter = 0
     for link in links:
         responce = requests.Session().get(link.get('href'), headers=headers, cookies=cookies)
-        time.sleep(random.randint(1, 5))
-        counter += 1
-        with open(f"rentanalyser/data/cian_page_flat№{counter}.html", "w", encoding="utf8") as file:
-            file.write(responce.text)
-        # soap = BeautifulSoup(responce.text, 'lxml')
-        # price = soap.find("div", class_="xa15a2ab7--fc68b9--amount").find("span").text
-        
-        # break
-
-#обработка каждой html-страницы из папки /data
-def page_parsing():
-    for counter in range(1, 29):
-        with open(f"rentanalyser/data/cian_page_flat№{counter}.html", "r", encoding="utf-8") as file:
-            soap = BeautifulSoup(file, 'lxml')
-            price1div = soap.find("div", class_="xa15a2ab7--e39360--aside")
-            if price1div:
-                price2div = price1div.find("div", class_="xa15a2ab7--fc68b9--amount")
-                if price2div:
-                    price = price2div.find("span").text
-            adress1div = soap.find("div", class_="xa15a2ab7--_3e6a9--header-information")
-            if adress1div:
-                adress2div = adress1div.find("span")
-                if adress2div:
-                    adress = adress2div.get('content')
-            
-            square1div = soap.find("div", class_="xa15a2ab7--_2be4c--text")
-            square = None
-            if square1div:
-                square2div = square1div.find("span", style="letter-spacing: -0.2px;", class_="xa15a2ab7--_7735e--color_text-primary-default xa15a2ab7--_2697e--lineHeight_6u xa15a2ab7--_2697e--fontWeight_bold xa15a2ab7--_2697e--fontSize_16px xa15a2ab7--_17731--display_block xa15a2ab7--dc75cc--text")
-                if square2div:
-                    square = square2div.text
-            description1div = soap.find("div", class_="xa15a2ab7--_3e6a9--header-information")
-            if description1div:
-                description = description1div.find("h1").text
-                #записываем кортеж файлов. . .
-            with open("full_data.csv", "a", encoding="utf-8") as file:
-                writer = csv.writer(file)
-                writer.writerow((description, adress, square, price))
+        page_parsing(responce.text) 
+        time.sleep(random.randint(2, 4))
 
 
+#обработка каждой html-страницы
+def page_parsing(source):
+    soap = BeautifulSoup(source, 'lxml')
+    
+    try:
+        price =soap.find("div", class_="xa15a2ab7--fc68b9--amount").find("span").text
+        price = price.split()[0] + price.split()[1]
+    except:
+        price = "не найдено"
+
+    try:
+        header_info = soap.find("div", class_="xa15a2ab7--_3e6a9--header-information")
+        adress = header_info.find("span").get('content')
+    except:
+        adress = "не найдено"
+    
+    square = "не найдено"
+    floor = "не найдено"
+    building_year = "не найдено"
+    try:
+        block_under_pic = soap.find_all("span", class_="xa15a2ab7--_7735e--color_text-primary-default xa15a2ab7--_2697e--lineHeight_6u xa15a2ab7--_2697e--fontWeight_bold xa15a2ab7--_2697e--fontSize_16px xa15a2ab7--_17731--display_block xa15a2ab7--dc75cc--text")
+        for item in block_under_pic:
+            try:
+                prev_sibling = item.previous_sibling
+                while prev_sibling and (isinstance(prev_sibling, str) and prev_sibling.strip() == ""):
+                    prev_sibling = prev_sibling.previous_sibling
+                prev_text = prev_sibling.text
+                if prev_text == "Общая площадь":
+                    square = item.text.split()[0]
+                elif prev_text == "Этаж":
+                    floor = item.text 
+                elif prev_text == "Год постройки":
+                    building_year = item.text
+            except:
+                continue
+    except:
+        print("Ошибка block_under_pic")
+    
+    try:
+        rooms = soap.find("div", class_="xa15a2ab7--_3e6a9--header-information").find("h1").text.split()[1][0]
+        if rooms == "с":
+            rooms = "студия"
+    except:
+        rooms = "не найдено"
+
+    #записываем кортеж файлов. . .
+    with open("full_data.csv", "a", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow((adress, rooms, square, floor, building_year, price))
 
 #main-функция
 def main():
-    page_parsing()
+    url = "https://nn.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&p=1&region=4885&type=4"
+    responce = requests.Session().get(url, headers=headers)
+    cookies = responce.cookies.get_dict()
+    time.sleep(random.randint(2, 4))
 
+    for counter in range(1,45):
+        url = f"https://nn.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&p={counter}&region=4885&type=4"
+        flat_pages__download(main_page_download(counter, url,cookies), cookies)
+    return 0
 #исполнение программы, вызовом главной функции
 main()
+
 
